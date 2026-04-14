4168fabcfacab5d226d08323ac82f67d7d45223e
Arthur Lopes <artglopes@gmail.com>
2026-04-14 18:30:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project-boost
+First of three games taught on GameDev.tv's Udemy course.
---
+++ player.gd
@@ -0,0 +1,17 @@
+extends Node3D # Acesses everything on the node it's attached to
+
+# Called when the node enters the scene tree for the first time.
+
+# Essentially just a "start" to the program
+func _ready() -> void:
+	# Just clowning on ArcSys, hahaha.
+	print("Do dreams come true...? NIGHTMARE ONE! COMMENCE!")
+	# Following the course's orders... my way.
+	print("DO NOT DISMAY, LIFE GOES ON")
+	print("7x6")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Essentially works as a tick rate, which is how it checks for frame updates.
+func _process(delta: float) -> void:
+	pass
